ae2b1a9237bd4ad9b693497ef5c0c2f8f7a2fecf
AlexOS12 <sluhach2016@gmail.com>
2024-06-03 22:15:37 +0300

    Лаб 5 готова

---
+++ Lab5/app/action_logs.py
@@ -0,0 +1,111 @@
+import math
+import io
+from flask import Blueprint,  render_template, request, send_file
+from app import db_connector
+from mysql.connector.errors import DatabaseError
+
+
+logs_bp = Blueprint('logs', __name__, url_prefix='/logs')
+
+PAGE_COUNT = 10
+
+def generate_file(fields, records):
+    result = ','.join(fields) + '\n'
+    for record in records:
+        line = ','.join([str(getattr(record, field, '') or '') for field in fields]) + '\n'
+        result += line 
+    return io.BytesIO(result.encode())
+
+@logs_bp.route('/')
+def index():
+    logs = []
+    page_number = request.args.get('page_number', 1, type = int)
+    
+    try:
+        db_connection = db_connector.connect()
+        with db_connection.cursor(named_tuple=True) as cursor:
+            query = ("SELECT visit_logs.id, users.login, visit_logs.path, visit_logs.created_at "
+                     "FROM visit_logs LEFT JOIN users ON visit_logs.user_id = users.id "
+                     "ORDER BY visit_logs.created_at DESC "
+                     f"LIMIT {PAGE_COUNT} OFFSET {PAGE_COUNT*(page_number - 1)} ")
+            cursor.execute(query)
+            logs = cursor.fetchall()
+
+            query = ("SELECT COUNT(*) as count FROM visit_logs")
+            cursor.execute(query)
+            total_count = cursor.fetchone().count
+            all_page = math.ceil(total_count / PAGE_COUNT)
+            start_page = max(page_number - 2, 1)
+            end_page = min(page_number + 2, all_page)
+
+            return render_template("action_logs.html", logs=logs, start_page=start_page, end_page=end_page, page_number=page_number)
+    except DatabaseError as error:
+        print(f"Произошла ошибка БД: {error}")
+
+@logs_bp.route('/users_stat')
+def users_stat():
+    logs = []    
+    page_number = request.args.get('page_number', 1, type = int)
+    try:
+        db_connection = db_connector.connect()
+        with db_connection.cursor(named_tuple=True) as cursor:
+            query = ("SELECT users.login, COUNT(*) as visit_count "
+            "FROM visit_logs LEFT JOIN users on visit_logs.user_id = users.id "
+            "GROUP BY users.id "
+            "ORDER BY visit_count desc "
+            f"LIMIT {PAGE_COUNT} OFFSET {PAGE_COUNT*(page_number - 1)}" )
+            cursor.execute(query)
+            logs = cursor.fetchall()
+
+            query = ("SELECT COUNT(*) as count from (SELECT DISTINCT user_id from visit_logs) as uniq_users")
+            cursor.execute(query)
+            total_count = cursor.fetchone().count
+
+            all_page = math.ceil(total_count / PAGE_COUNT)
+            start_page = max(page_number - 2, 1)
+            end_page = min(page_number + 2, all_page)
+
+            if request.args.get('download'):
+                file = generate_file(['login', 'visit_count'], logs)
+                return send_file(file, mimetype='text/csv', as_attachment=True, download_name='user_stat.csv')
+
+            return render_template("users_stat.html", logs=logs, start_page=start_page, end_page=end_page, page_number=page_number)
+    except DatabaseError as error:
+        print(f"Произошла ошибка БД: {error}")    
+
+
+@logs_bp.route('/pages_stat')
+def pages_stat():
+    logs = []
+    page_number = request.args.get('page_number', 1, type = int)
+
+    try:
+        db_connection = db_connector.connect()
+        with db_connection.cursor(named_tuple=True) as cursor:
+            query = ("SELECT path, COUNT(*) as visit_count from visit_logs "
+                     "group by path "
+                     "order by visit_count desc "
+                     f"LIMIT {PAGE_COUNT} OFFSET {PAGE_COUNT*(page_number - 1)}"
+            )
+            cursor.execute(query)
+            logs = cursor.fetchall()
+
+            query = ("SELECT COUNT(*) as count from (SELECT DISTINCT path from visit_logs) as uniq_paths")
+            cursor.execute(query)
+            total_count = cursor.fetchone().count
+
+            all_page = math.ceil(total_count / PAGE_COUNT)
+            start_page = max(page_number - 2, 1)
+            end_page = min(page_number + 2, all_page)
+
+            if request.args.get('download'):
+                file = generate_file(['path', 'visit_count'], logs)
+                return send_file(file, mimetype='text/csv', as_attachment=True, download_name='pages_stat.csv')
+
+            return render_template("pages_stat.html", logs=logs, start_page=start_page, end_page=end_page, page_number=page_number)
+    except DatabaseError as error:
+        print(f"Произошла ошибка БД: {error}")
+
+    return render_template("logs_base.html")
+
+    
---
+++ Lab5/app/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, session, request, redirect, url_for, flash
+from flask_login import login_required, current_user
+from mysqldb import DBConnector
+from mysql.connector.errors import DatabaseError
+
+app = Flask(__name__)
+application = app
+app.config.from_pyfile('config.py')
+
+db_connector = DBConnector(app)
+
+from authorization import bp as authorization_bp, init_login_manager
+app.register_blueprint(authorization_bp)
+init_login_manager(app)
+
+from users import bp as users_bp
+app.register_blueprint(users_bp)
+
+from action_logs import logs_bp
+app.register_blueprint(logs_bp)
+
+@app.before_request
+def user_actions_logger():
+    print(request.endpoint)
+
+    if request.endpoint == 'static' or request.path == '/favicon.ico':
+        return
+
+    url = request.path
+    user_id = current_user.id if current_user.is_authenticated else None
+    
+    try:
+        db_connection = db_connector.connect()
+        with db_connection.cursor(named_tuple=True) as cursor:
+            cursor.execute("INSERT INTO visit_logs (user_id, path) VALUES (%s, %s)", (user_id, url))
+            db_connection.commit()
+    except DatabaseError as error:
+        print(f"Произошла ошибка БД: {error}")
+        if db_connection:
+            db_connection.rollback()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    session['counter'] = session.get('counter', 0) + 1
+
+    return render_template('info.html')
+
+@app.route('/secret')
+@login_required
+def secret():
+    return render_template('secret.html')
+
+# python -m venv ve
+# . ve/bin/activate -- Linux
+# ve\Scripts\activate -- Windows
+# pip install flask python-dotenv
